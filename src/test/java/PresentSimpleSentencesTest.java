681a89c2f133d147f4f0e664c6f0079e57bc4cb9
Roman Kaminskiy - notebook <derrki@gmail.com>
2019-02-01 03:05:12 +0200

    Вніс зміни в імена тестів. Добавив декілька тестів з різними комбінаціями вхідних аргументів.

--- src/test/java/PresentSimpleSentencesTest.java
+++ src/test/java/PresentSimpleSentencesTest.java
@@ -11,9 +11,23 @@ public class PresentSimpleSentencesTest {
     }
 
     @Test
-    public void buildDeclarativeSenttencesTestOneWords(){
+    public void build_Declarative_Senttences_Test_First_Word_Is_Null(){
+        String expected = "Не введено всі члени речення";
+        String actual = PresentSimpleSentences.buildDeclarativeSenttences(null, "want");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void build_Declarative_Senttences_Test_Second_Word_Is_Null(){
         String expected = "Не введено всі члени речення";
         String actual = PresentSimpleSentences.buildDeclarativeSenttences("I", null);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void build_Declarative_Senttences_Test_Two_Words_Is_Null(){
+        String expected = "Не введено всі члени речення";
+        String actual = PresentSimpleSentences.buildDeclarativeSenttences(null, null);
         Assert.assertEquals(expected, actual);
     }
 }
