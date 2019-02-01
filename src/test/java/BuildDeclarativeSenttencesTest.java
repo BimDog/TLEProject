387e1d17cfeb9ad566f38498a05be3d878538bcc
Roman Kaminskiy - notebook <derrki@gmail.com>
2019-02-01 03:24:53 +0200

    Рефакторинг імен тестів.

--- src/test/java/BuildDeclarativeSenttencesTest.java
+++ src/test/java/BuildDeclarativeSenttencesTest.java
@@ -1,31 +1,31 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class PresentSimpleSentencesTest {
+public class BuildDeclarativeSenttencesTest {
 
     @Test
-    public void buildDeclarativeSenttencesTestTwoWords(){
+    public void test_correct_two_words(){
         String expected = "I want";
         String actual = PresentSimpleSentences.buildDeclarativeSenttences("I", "want");
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void build_Declarative_Senttences_Test_First_Word_Is_Null(){
+    public void test_first_word_is_null(){
         String expected = "Не введено всі члени речення";
         String actual = PresentSimpleSentences.buildDeclarativeSenttences(null, "want");
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void build_Declarative_Senttences_Test_Second_Word_Is_Null(){
+    public void test_second_word_is_null(){
         String expected = "Не введено всі члени речення";
         String actual = PresentSimpleSentences.buildDeclarativeSenttences("I", null);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void build_Declarative_Senttences_Test_Two_Words_Is_Null(){
+    public void test_two_words_is_null(){
         String expected = "Не введено всі члени речення";
         String actual = PresentSimpleSentences.buildDeclarativeSenttences(null, null);
         Assert.assertEquals(expected, actual);
