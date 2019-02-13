7a9f6584ac74a3147f88a001be7dcc3922b87495
Roman Kaminskiy - notebook <derrki@gmail.com>
2019-02-14 00:53:14 +0200

    Експеремент з Мапою в методі main(). Вивід пар слово - переклад.

--- src/main/java/ListWord.java
+++ src/main/java/ListWord.java
@@ -1,2 +1,5 @@
 public class ListWord {
+    public static int getList() {
+        return 10;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
+        String[] wordsE = new String[]{"want", "like", "need", "do", "live", "work", "know", "get", "see", "hear"};
+        String[] wordsU = new String[]{"хотіти", "любити", "потребувати", "робити", "жити", "працювати", "знати", "ставати", "бачити", "чути"};
 
+        Map<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < wordsE.length; i++) {
+            map.put(wordsE[i], wordsU[i]);
         }
+
+        for (String key: map.keySet()) {
+
+            System.out.print(key + " - ");
+            System.out.println(map.get(key));
+        }
+
+
+    }
 }
